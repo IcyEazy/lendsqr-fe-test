6a80153f5af5d44bd2053f0abb29516dfab01464
Adigun Israel Collins <adigunisrael@gmail.com>
2024-06-21 20:25:04 +0100

    updated the endpoint

--- src/api/Users.tsx
+++ src/api/Users.tsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const getAllUsers = async () => {
   const response = await axios.get(
-    // "https://run.mocky.io/v3/d2bb9973-01f6-4ef1-889c-f5cd8cda2701"
-    "http://localhost:3005/users"
+    "https://run.mocky.io/v3/cee76889-f53b-4d1d-8dc5-b130abb13c54"
+    // "http://localhost:3005/users"
   );
   return response.data;
 };
